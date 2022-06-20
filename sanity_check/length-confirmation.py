80769a7296474e40416f5a00f5ebe1cd52b47e62
Zeynep AKI <73976500+zeynepaki@users.noreply.github.com>
2022-06-20 13:30:35 +0100

    fixed typo

--- sanity_check/length-confirmation.py
+++ sanity_check/length-confirmation.py
@@ -14,22 +14,22 @@ nairobi_compact = pd.read_csv('output_cities/nairobi_compact.csv')
 paris_og = pd.read_csv('Paris.csv')
 paris_compact = pd.read_csv('output_cities/paris_compact.csv')
 
-confimration = len(copenhagen_og.index)/4
+confirmation = len(copenhagen_og.index)/4
 print('Copenhagen OG length: ', len(copenhagen_og.index))
 print('Copenhagen Compact length: ', len(copenhagen_compact.index))
-print('Copenhagen OG/4: ', str(confimration))
-confimration = len(johanesberg_og.index)/4
+print('Copenhagen OG/4: ', str(confirmation))
+confirmation = len(johanesberg_og.index)/4
 print('Johanesberg OG length: ', len(johanesberg_og.index))
 print('Johanesberg Compact length: ', len(johanesberg_compact.index))
-print('Johanesberg OG/4: ', str(confimration))
-confimration = len(madrid_og.index)/4
+print('Johanesberg OG/4: ', str(confirmation))
+confirmation = len(madrid_og.index)/4
 print('Madrid OG length: ', len(madrid_og.index))
 print('Madrid Compact length: ', len(madrid_compact.index))
-print('Madrid OG/4: ', str(confimration))
-confimration = len(nairobi_og.index)/4
+print('Madrid OG/4: ', str(confirmation))
+confirmation = len(nairobi_og.index)/4
 print('Nairobi OG length: ', len(nairobi_og.index))
 print('Nairobi Compact length: ', len(nairobi_compact.index))
-print('Nairobi OG/4: ', str(confimration))
-confimration = len(paris_og.index)/4
+print('Nairobi OG/4: ', str(confirmation))
+confirmation = len(paris_og.index)/4
 print('Paris OG length: ', len(paris_og.index))
 print('Paris Compact length: ', len(paris_og.index))
